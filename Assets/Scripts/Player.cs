3e9da27e73eec9c8463448574a86c589673f13b9
Bogdan <bogdancenusa19@gmail.com>
2024-10-20 21:20:49 +0300

    Initial Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player: MonoBehaviour
+{
+
+    #region Components
+    public Animator anim { get; private set; }
+    
+    #endregion
+    #region State
+    public PlayerStateMachine stateMachine { get; private set; }
+    
+    public PlayerIdleState idleState { get; private set; }
+    public PlayerMoveState moveState { get; private set; }
+    
+    #endregion
+
+    private void Awake()
+    {
+        stateMachine = new PlayerStateMachine();
+
+        idleState = new PlayerIdleState(this, stateMachine, "Idle");
+        moveState = new PlayerMoveState(this, stateMachine, "Move");
+    }
+
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        
+        stateMachine.Initialize(idleState);
+    }
+
+    private void Update()
+    {
+        stateMachine.currentState.Update();
+    }
+}
